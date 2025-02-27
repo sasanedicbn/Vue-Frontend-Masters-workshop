1a640f325a9b42e0a650dcf016055c48ebf79404
Sasa Nedic <svetasmak0@gmail.com>
2025-02-27 23:53:07 +0100

    Add data in component

--- Vue-project/src/components/HelloWorld.vue
+++ Vue-project/src/components/HelloWorld.vue
@@ -1,6 +1,14 @@
 
-<script></script>
+<script>
+export default {
+  data(){
+    return{
+    counter:0}
+  }
+}
+</script>
 
 <template>
   <h1>Hello from my frist Vue component</h1>
+  <span>{{ counter }}</span>
 </template>
