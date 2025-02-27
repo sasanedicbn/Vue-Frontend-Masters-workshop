095f2453cd6313bed735b2e21d4aac63a039c520
Sasa Nedic <svetasmak0@gmail.com>
2025-02-27 23:54:11 +0100

    Add method in vue component

--- Vue-project/src/components/HelloWorld.vue
+++ Vue-project/src/components/HelloWorld.vue
@@ -4,11 +4,17 @@ export default {
   data(){
     return{
     counter:0}
+  },
+  methods:{
+    increaseCounter(){
+      this.counter++
+    }
   }
 }
 </script>
 
 <template>
   <h1>Hello from my frist Vue component</h1>
+  <button>+</button>
   <span>{{ counter }}</span>
 </template>
