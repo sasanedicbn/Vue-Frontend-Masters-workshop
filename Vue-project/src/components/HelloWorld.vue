da5d4077a59752526515354ddc9f19e66fd59857
Sasa Nedic <svetasmak0@gmail.com>
2025-02-28 00:28:06 +0100

    Play with some stuff

--- Vue-project/src/components/HelloWorld.vue
+++ Vue-project/src/components/HelloWorld.vue
@@ -4,20 +4,18 @@ export default {
   data(){
     return{
     counter:0,
-    errorMessage:'Ooops, you cant go in negative numbers'
+    errorMessage:'Ooops, you cant go in negative numbers',
+    characters: ['Jovan', 'Ilija', 'Radovan', 'Sasa']
   }
   },
   methods:{
     increaseCounter(){
       this.counter++
-    }
+    },
     decreaseCounter(){
-      if(this.counter > 0){
+      if(this.counter < 0) return
         this.counter--
-      } else{
-
-      }
-    }
+    },
   }
 }
 </script>
@@ -25,7 +23,8 @@ export default {
 <template>
   <h1>Hello from my frist Vue component</h1>
   <button @click="increaseCounter">+</button>
-  <button @click="increaseCounter">+</button>
+  <button @click="decreaseCounter">-</button>
   
   <span>{{ counter }}</span>
+  <p v-if="counter < 0">{{ errorMessage }}</p>
 </template>
