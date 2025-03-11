3e6b144ca85fb8fe367f4508a9938f24d4eaf9df
Sasa Nedic <svetasmak0@gmail.com>
2025-03-11 23:10:45 +0100

    Create beforeCreate

--- Vue-project/src/App.vue
+++ Vue-project/src/App.vue
@@ -15,7 +15,11 @@ export default {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
     const data = await response.json();
     this.pokemon = data.results;
-  }
+   },
+  },
+  beforeCreate(){
+    console.log('------before create--------')
+    console.log(this.pokemon)
   }
 };
 </script>
