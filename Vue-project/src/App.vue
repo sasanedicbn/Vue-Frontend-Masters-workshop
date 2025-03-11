bfc6102080d30406ed2fb237354194cf937d0d0a
Sasa Nedic <svetasmak0@gmail.com>
2025-03-11 22:55:15 +0100

    Create methods for fetchPokemon

--- Vue-project/src/App.vue
+++ Vue-project/src/App.vue
@@ -5,26 +5,34 @@ export default {
   // components: {
   //   HelloWorld
   // },
-  data () {
+  data() {
     return {
-      pokemon: [1,2,3]
-    }
+      pokemon: [1, 2, 3]
+    };
   },
+  methods:{
+    async fetchPokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const data = await response.json();
+    this.pokemon = data.results;
+  }
+  }
 };
 </script>
 
 <template>
   <!-- <HelloWorld/> -->
-   <h2>New App</h2>
+  <h2>New App</h2>
   <ul>
-    <li v-for="(fav, _) of pokemon" >{{ fav }}</li>
+    <li v-for="(fav, index) in pokemon" :key="index">-{{ fav }}</li>
   </ul>
+  <button @click="fetchPokemon">ADD BTN</button>
 </template>
 
 <style scoped>
 /* Stilovi */
 </style>
-
+<!-- https://pokeapi.co/api/v2/pokemon?limit=151 -->
 
 
 
