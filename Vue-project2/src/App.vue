71004473449d4381aa93a4a3456dc27d8c8ed400
Sasa Nedic <svetasmak0@gmail.com>
2025-03-12 13:17:12 +0100

    Start

--- Vue-project2/src/App.vue
+++ Vue-project2/src/App.vue
@@ -3,10 +3,30 @@
 </script>
 
 <template>
-
-<p>SASA</p>
+<header>
+  <nav>
+    <a href="HomePage" >Home</a>
+    <a href="LoginPage" >Log in</a>
+    <a href="UsersPage" >Users</a>
+  </nav>
+</header>
 </template>
 
 <style >
-
+header{
+  background-color: red;
+  height: 40px;
+  padding: 0 64px;
+}
+nav{
+  display: flex; 
+  justify-content: flex-end;
+  align-items: center;
+  gap: 34px;
+}
+a{
+    color: black;
+    text-decoration: none;
+    font-size: 20px;
+  }
 </style>
