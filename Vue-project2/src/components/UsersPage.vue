f2f6580ec293f5a25dc00dd87b40f331435f78ea
Sasa Nedic <svetasmak0@gmail.com>
2025-03-12 17:59:21 +0100

    Create UsersPage component

---
+++ Vue-project2/src/components/UsersPage.vue
@@ -0,0 +1,29 @@
+<script>
+export default({
+    data: () => {
+        return{
+            usersList:[]
+        }
+    },
+    methods:{
+        async fetchUserList()  {
+         this.usersList = await fetch('https://jsonplaceholder.typicode.com/users')
+         .then(response => response.json())
+        }
+    },
+    created() {
+      this.fetchUserList()
+    }
+})
+</script>
+
+<template>
+ <div>
+   <ul>
+     <li v-for="(user, index) of usersList" :key="index">{{ usersList }}</li>
+   </ul>
+ </div>
+</template>
+
+<style>
+</style>
