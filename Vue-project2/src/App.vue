7a5d1c0a60bc208cbe91c5c0ded8121a7740a428
Sasa Nedic <svetasmak0@gmail.com>
2025-03-12 13:54:19 +0100

    Create Home Page component

--- Vue-project2/src/App.vue
+++ Vue-project2/src/App.vue
@@ -1,32 +1,65 @@
 <script >
+import HomePage from './components/HomePage.vue';
 
+export default{
+  components:{
+    HomePage,
+  }
+}
 </script>
 
 <template>
 <header>
+  <p class="logo">VUE</p>
   <nav>
     <a href="HomePage" >Home</a>
     <a href="LoginPage" >Log in</a>
     <a href="UsersPage" >Users</a>
   </nav>
 </header>
+<div class="content-wrapper">
+  <HomePage/>
+</div>
 </template>
-
 <style >
+body{
+  margin: 0;
+
+}
 header{
-  background-color: red;
-  height: 40px;
+  background-color: rgb(240, 236, 236);
+  height: 52.5px;
   padding: 0 64px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90.6%;
+  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 nav{
   display: flex; 
   justify-content: flex-end;
   align-items: center;
   gap: 34px;
+  height: 100%;
 }
 a{
-    color: black;
+    color: rgb(71, 69, 69);
     text-decoration: none;
-    font-size: 20px;
+    font-size: 18px;
+  }
+  .logo{
+    font-size: 22px;
+    font-weight: bold;
+    line-height: 1.2;
+    color: rgb(8, 128, 8);
+  }
+  .content-wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin:0 20rem;
+    flex-direction: column;
+    
   }
 </style>
---
+++ Vue-project2/src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+    name: 'HomePage'
+}
+</script>
+
+<template>
+<h2>Welcome to Home Page</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Impedit magni veniam nam ut quidem earum ad distinctio, necessitatibus quis labore dolores 
+facilis odio inventore, rem consectetur temporibus unde sunt assumenda.</p>
+</template>
+
+<style>
+h2{
+    color: rgb(97, 95, 95);
+}
+p{
+    color: rgb(158, 157, 157);
+}
+</style>
