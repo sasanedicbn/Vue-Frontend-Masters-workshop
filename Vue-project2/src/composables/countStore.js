260fab7f9a0302d77070ba7b5e19ad20b6587478
Sasa Nedic <svetasmak0@gmail.com>
2025-03-23 21:33:44 +0100

    Create controlData function

--- Vue-project2/src/composables/countStore.js
+++ Vue-project2/src/composables/countStore.js
@@ -1,6 +1,12 @@
 import { ref } from "vue";
 
 const countValue = ref(0);
-export const incrementCounter = () => countValue.value++;
+export function controlData() {
+  const innerValue = ref(10);
+  const incrementCounter = () => {
+    innerValue.value++;
+  };
+  return { innerValue, incrementCounter };
+}
 
 export default countValue;
