9e4c2dd67d4919cc7fc24a6174891910d25ce79f
Sasa Nedic <svetasmak0@gmail.com>
2025-03-26 21:20:31 +0100

    Practice Programmatic Navigation

--- Vue-project2/src/views/HomePage.vue
+++ Vue-project2/src/views/HomePage.vue
@@ -1,30 +1,46 @@
 <script setup>
-import BaseButton from '@/Base/Base-button.vue';
-import {usersData} from '../composables/usersStore'
-import UserCard from '../components/UserCard.vue';
+import BaseButton from "@/Base/Base-button.vue";
+import { usersData } from "../composables/usersStore";
+import UserCard from "../components/UserCard.vue";
+import controlData from "../composables/countStore";
+import { useRouter } from "vue-router";
+import { watch } from "vue";
+const { innerValue, incrementCounter } = controlData();
+console.log("innerCount", innerValue.value);
+const router = useRouter();
+
+watch(innerValue, () => {
+  if (innerValue.value > 12) {
+    router.push("/users");
+  }
+});
 </script>
 
-<template >
-<div :class="$style.wrapper">
-<h2>Welcome to Home Page</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Impedit magni veniam nam ut quidem earum ad distinctio, necessitatibus quis labore dolores 
-facilis odio inventore, rem consectetur temporibus unde sunt assumenda.</p>
-<BaseButton text="SUBMIT" type="warning"/>
-<ul>
-    <li v-for="user in usersData" :key="user.id">
+<template>
+  <div :class="$style.wrapper">
+    <h2>Welcome to Home Page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magni
+      veniam nam ut quidem earum ad distinctio, necessitatibus quis labore
+      dolores facilis odio inventore, rem consectetur temporibus unde sunt
+      assumenda.
+    </p>
+    <p>Vrijednost value: {{ innerValue }}</p>
+    <button @click="incrementCounter()">Increment</button>
+    <BaseButton text="SUBMIT" type="warning" />
+    <ul>
+      <li v-for="user in usersData" :key="user.id">
         <UserCard :user="user" />
-     </li>
-</ul>
-</div>
-
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style module>
-h2{
-    color: rgb(97, 95, 95);
+h2 {
+  color: rgb(97, 95, 95);
 }
-p{
-    color: rgb(158, 157, 157);
+p {
+  color: rgb(158, 157, 157);
 }
 </style>
